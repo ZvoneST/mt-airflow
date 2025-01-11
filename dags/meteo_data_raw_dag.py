ca7a0fc3a97f53da55fb098fac76456c66b7507d
ZvoneST <zstipic1989@gmail.com>
2025-01-11 19:08:38 +0000

    meteo data dag

---
+++ dags/meteo_data_raw_dag.py
@@ -0,0 +1,79 @@
+import sys
+import os
+import json
+import psycopg2
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+from include.api_processor import APIProcessor
+from include.request_methods import meteo_data_request
+
+from datetime import datetime, timedelta
+from airflow.models import DAG
+from airflow.hooks.base import BaseHook
+from airflow.providers.http.sensors.http import HttpSensor
+from airflow.providers.common.sql.operators.sql import SQLExecuteQueryOperator
+from airflow.operators.python import PythonOperator
+
+source_conn = BaseHook.get_connection('farm_management_conn')
+target_conn = BaseHook.get_connection('raw_zone_conn')
+
+api_conn = BaseHook.get_connection('meteo_data_conn')
+
+conn_fm = psycopg2.connect(
+    dbname=source_conn.schema,
+    user=source_conn.login,
+    password=source_conn.password,
+    host=source_conn.host,
+    port=source_conn.port,
+)
+
+conn_landing = psycopg2.connect(
+    dbname=target_conn.schema,
+    user=target_conn.login,
+    password=target_conn.password,
+    host=target_conn.host,
+    port=target_conn.port,
+)
+
+meteo_query = '''
+    SELECT meteo_location_id, meteo_coordinates_wkt 
+    FROM management.meteo_locations;
+'''
+insert_query = '''
+    INSERT INTO landing.meteo_data (meteo_location_id, meteo_data)
+    VALUES (%s, %s)
+    ON CONFLICT (meteo_location_id) DO UPDATE
+    SET meteo_data = EXCLUDED.meteo_data;
+'''
+
+api_processor = APIProcessor(source_conn=conn_fm, target_conn=conn_landing,
+                             fetch_query=meteo_query, insert_query=insert_query,
+                             request_method=meteo_data_request, api_key=api_conn.login)
+
+default_args = {
+    'owner': 'admin',
+    'start_date': datetime(2025, 1, 1),
+    'retries': 1,
+}
+
+with DAG(
+    dag_id='meteo_data_raw',
+    default_args=default_args,
+    schedule='0 3 * * *',
+    catchup=False
+) as dag:
+    
+    # meteo_api_active = HttpSensor(
+    #     task_id='meteo_api_active',
+    #     http_conn_id='meteo_data_conn',
+    #     endpoint='/v2/weather',
+    #     headers=api_conn.login
+    # )
+    
+    get_store_meteo_data = PythonOperator(
+        task_id='get_store_meteo_data',
+        python_callable=api_processor.process_data
+    )
+    
+    
+    get_store_meteo_data
