acfab1ecb48f3b4586bd0adccd41a1b106e12faf
ZvoneST <zstipic1989@gmail.com>
2025-01-11 19:07:47 +0000

    refactor request methods

--- include/request_methods.py
+++ include/request_methods.py
@@ -1,6 +1,8 @@
 import requests
+from datetime import datetime, timedelta
 
-def send_soilgrids_request(lon, lat):
+
+def soilgrids_request(lon, lat):
     url = 'https://rest.isric.org/soilgrids/v2.0/properties/query'
     parameters = {
         'lon': lon,
@@ -12,5 +14,25 @@ def send_soilgrids_request(lon, lat):
         'value': ['mean', 'uncertainty']
     }
     response = requests.get(url, params=parameters)
+    response.raise_for_status()
+    return response.json()
+
+
+def meteo_data_request(lon, lat, key):
+    url = 'https://api.stormglass.io/v2/weather/point'
+    end = datetime.now().date()
+    start = end - timedelta(days=1)
+    headers = {
+        'Authorization': key
+    }
+    parameters = {
+        'lat': lat,
+        'lng': lon, 
+        'params': 'airTemperature,pressure,humidity,precipitation',
+        'start': start.isoformat(),
+        'end': end.isoformat(),
+        'source': 'sg'
+    }
+    response = requests.get(url, params=parameters, headers=headers)
     response.raise_for_status()
     return response.json()
