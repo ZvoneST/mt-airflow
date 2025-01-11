d2040bd8209f32cf397bc24d7863bcff0b6d3947
ZvoneST <zstipic1989@gmail.com>
2025-01-11 15:14:50 +0000

    feractor - prettifier

--- include/request_methods.py
+++ include/request_methods.py
@@ -1,15 +1,15 @@
 import requests
 
 def send_soilgrids_request(lon, lat):
-    url = "https://rest.isric.org/soilgrids/v2.0/properties/query"
+    url = 'https://rest.isric.org/soilgrids/v2.0/properties/query'
     parameters = {
-        "lon": lon,
-        "lat": lat,
-        "property": [
-            "bdod", "cec", "clay", "nitrogen", "phh2o", "sand", "silt", "soc"
+        'lon': lon,
+        'lat': lat,
+        'property': [
+            'bdod', 'cec', 'clay', 'nitrogen', 'phh2o', 'sand', 'silt', 'soc'
         ],
-        "depth": ["0-5cm", "0-15cm", "15-30cm"],
-        "value": ["mean", "uncertainty"]
+        'depth': ['0-5cm', '0-15cm', '15-30cm'],
+        'value': ['mean', 'uncertainty']
     }
     response = requests.get(url, params=parameters)
     response.raise_for_status()
