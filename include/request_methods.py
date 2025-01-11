4c1ffb887f58a9caa2a818331d2a8a169b3adf62
ZvoneST <zstipic1989@gmail.com>
2025-01-11 14:57:51 +0000

    soil grid api request method

---
+++ include/request_methods.py
@@ -0,0 +1,16 @@
+import requests
+
+def send_soilgrids_request(lon, lat):
+    url = "https://rest.isric.org/soilgrids/v2.0/properties/query"
+    parameters = {
+        "lon": lon,
+        "lat": lat,
+        "property": [
+            "bdod", "cec", "clay", "nitrogen", "phh2o", "sand", "silt", "soc"
+        ],
+        "depth": ["0-5cm", "0-15cm", "15-30cm"],
+        "value": ["mean", "uncertainty"]
+    }
+    response = requests.get(url, params=parameters)
+    response.raise_for_status()
+    return response.json()
