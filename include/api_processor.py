6df9723c4524617511847d37a539389213356c7d
ZvoneST <zstipic1989@gmail.com>
2025-01-11 15:14:02 +0000

    logging

--- include/api_processor.py
+++ include/api_processor.py
@@ -1,5 +1,13 @@
 import re
 import json
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
+)
+
+logger = logging.getLogger(__name__)
 
 
 class APIProcessor:
@@ -11,14 +19,12 @@ class APIProcessor:
         self.request_method = request_method
 
     def fetch_field_ids(self):
-        """Fetch field IDs and WKT coordinates from the source database."""
         with self.source_conn.cursor() as cursor:
             cursor.execute(self.fetch_query)
             return cursor.fetchall()
 
     @staticmethod
     def parse_wkt_coordinates(wkt):
-        """Extract longitude and latitude from WKT point."""
         match = re.match(r"POINT\s*\(\s*([-\d.]+)\s+([-\d.]+)\s*\)", wkt)
         if match:
             lon, lat = float(match.group(1)), float(match.group(2))
@@ -27,7 +33,6 @@ class APIProcessor:
             raise ValueError(f"Invalid WKT format: {wkt}")
 
     def store_raw_data(self, field_id, requested_data):
-        """Store the fetched SoilGrids data into the target database."""
         try:
             with self.target_conn.cursor() as cursor:
                 cursor.execute(self.insert_query, (field_id, json.dumps(requested_data)))
@@ -37,21 +42,20 @@ class APIProcessor:
             raise e
 
     def process_data(self):
-        """Fetch field IDs, send SoilGrids requests, and store the data."""
         try:
             field_data = self.fetch_field_ids()
 
             for field_id, wkt in field_data:
                 try:
                     lon, lat = self.parse_wkt_coordinates(wkt=wkt)
-                    print(f"Processing field_id {field_id} with coordinates ({lon}, {lat})...")
+                    logger.info(f"Processing field_id {field_id} with coordinates ({lon}, {lat})...")
 
                     requested_data = self.request_method(lon, lat)
 
                     self.store_raw_data(field_id=field_id, requested_data=requested_data)
-                    print(f"Successfully processed field_id {field_id}.")
+                    logger.info(f"Successfully processed field_id {field_id}.")
                 except Exception as e:
-                    print(f"Failed to process field_id {field_id}: {e}")
+                    logger.error(f"Failed to process field_id {field_id}: {e}")
         finally:
             self.source_conn.close()
             self.target_conn.close()
