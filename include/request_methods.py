98a39c6af0f15f7be1a31aadf680889c5c31205b
ZvoneST <zstipic1989@gmail.com>
2025-01-12 08:15:21 +0000

    ameteo request method refactor- error handling

--- include/request_methods.py
+++ include/request_methods.py
@@ -18,21 +18,29 @@ def soilgrids_request(lon, lat):
     return response.json()
 
 
-def meteo_data_request(lon, lat, key):
-    url = 'https://api.stormglass.io/v2/weather/point'
-    end = datetime.now().date()
-    start = end - timedelta(days=1)
-    headers = {
-        'Authorization': key
-    }
-    parameters = {
-        'lat': lat,
-        'lng': lon, 
-        'params': 'airTemperature,pressure,humidity,precipitation',
-        'start': start.isoformat(),
-        'end': end.isoformat(),
-        'source': 'sg'
-    }
-    response = requests.get(url, params=parameters, headers=headers)
-    response.raise_for_status()
-    return response.json()
+def meteo_data_request(lon, lat, key, days=1):
+    try:
+        url = 'https://api.stormglass.io/v2/weather/point'
+        end = datetime.now().date()
+        start = end - timedelta(days=days)
+        
+        headers = {
+            'Authorization': key
+        }
+        parameters = {
+            'lat': lat,
+            'lng': lon,
+            'params': 'airTemperature,pressure,humidity,precipitation',
+            'start': start.isoformat(),
+            'end': end.isoformat(),
+            'source': 'sg'
+        }
+        
+        response = requests.get(url, params=parameters, headers=headers)
+        response.raise_for_status()
+        return response.json()
+
+    except requests.exceptions.RequestException as e:
+        print(f"Request failed: {e}")
+        raise
+    
