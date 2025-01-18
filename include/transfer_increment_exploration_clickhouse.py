c148d3a35a2835f4030b1c0aaa498a3530f3f89d
ZvoneST <zstipic1989@gmail.com>
2025-01-18 21:09:42 +0000

    refactor transfer incremental process

--- include/transfer_increment_exploration_clickhouse.py
+++ include/transfer_increment_exploration_clickhouse.py
@@ -17,31 +17,31 @@ def select_field_tasks_query(filter_date: str):
    return f'''
         SELECT 
             field_task_id, agrotehnical_operation_id, agro_organization_id, field_id, crop_id, variety_id, production_type_id, 
-            agent_id, season_id, due_date, measurement_unit_id, quantity_ha, total_quantity, created_on
+            agent_id, season_id, due_date, measurement_unit_id, quantity_ha, total_quantity
         FROM 
             farm_management.field_tasks
         WHERE 
-            created_on > {filter_date};'''
+            created_on > '{filter_date}';'''
 
 def select_meteo_data_query(filter_date: str):
     return f'''
         SELECT 
             meteo_location_id, air_temperature, humidity, 
-            precipitation, pressure, meteo_time, created_on
+            precipitation, pressure, meteo_time
         FROM 
             external_data.hourly_meteo_data
         WHERE 
-            created_on > {filter_date};'''
+            created_on > '{filter_date}';'''
     
 def select_agro_meteo_data_query(filter_date: str): 
     return f'''
         SELECT 
             meteo_location_id, soil_moisture, soil_moisture_10cm, 
-            soil_temperature, soil_temperature_10cm, meteo_time, created_on
+            soil_temperature, soil_temperature_10cm, meteo_time
         FROM 
             external_data.hourly_agro_meteo_data
         WHERE 
-            created_on > {filter_date};'''
+            created_on > '{filter_date}';'''
 
 
 class CKHSTransferIncrementData:
@@ -63,36 +63,42 @@ class CKHSTransferIncrementData:
             return None
         
     def _fetch_data_from_postgres(self, latest_date, table: str):
-        try:
-            engine = create_engine(self.pg_conn)
-            
+        try:         
             if table == 'field_tasks':
                 source_query = select_field_tasks_query(filter_date=latest_date)
-            elif table == 'meteo_data':
+            elif table == 'hourly_meteo_data':
                 source_query = select_meteo_data_query(filter_date=latest_date)
             else:
                 source_query = select_agro_meteo_data_query(filter_date=latest_date)
                 
-            with engine.connect() as connection:
-                df = pd.read_sql(source_query, connection)
-            
+            with self.pg_conn.cursor() as cursor:
+                cursor.execute(source_query)
+                rows = cursor.fetchall()
+
+                column_names = [desc[0] for desc in cursor.description]
+
+            df = pd.DataFrame(rows, columns=column_names)
+
             logger.info(f'Fetched {len(df)} rows from PostgreSQL.')
             return df
         except Exception as e:
             logger.error('Error fetching data from PostgreSQL:', exc_info=True)
             return None
+        finally:
+            if 'connection' in locals() and self.pg_conn:
+                self.pg_conn.close()
         
-    def _insert_data_to_clickhouse(self, df: pd.DataFrame):
+    def _insert_data_to_clickhouse(self, df: pd.DataFrame, table: str):
         try:
             client = chc.get_client(**self.ckhs_config)
             column_names = df.columns.tolist()
             data = df.values.tolist()
-            client.insert('field_tasks', data, column_names=column_names)
+            client.insert(table=table, data=data, column_names=column_names)
             logger.info(f'{len(data)} rows inserted into ClickHouse.')
         except Exception as e:
             logger.error('Error inserting data into ClickHouse:', exc_info=True)
     
-    def transfer_field_tasks(self, database: str, table: str):
+    def transfer_increment_data(self, database: str, table: str):
         latest_date = self._fetch_latest_created_on(database=database, table=table)
         if latest_date is None:
             logger.info('No existing data in ClickHouse. Fetching all data from PostgreSQL.')
@@ -101,25 +107,32 @@ class CKHSTransferIncrementData:
         df = self._fetch_data_from_postgres(latest_date=latest_date, table=table)
         
         if df is not None and not df.empty:
-            self._insert_data_to_clickhouse(df)
+            self._insert_data_to_clickhouse(df=df, table=table)
         else:
             logger.warning('No new data to insert.')
-        if 'connection' in locals() and self.pg_conn:
-            self.pg_conn.close()
+
+
+class CallableCKHSTransferIncrementData():
+    
+    def __init__(self, pg_conn: str, ckhs_config: dict, database: str = 'staging'):
+        self.transfer_data = CKHSTransferIncrementData(pg_conn=pg_conn, 
+                                                       ckhs_config=ckhs_config)
+        self.database = database
         
-    
-    
-    
+    def transfer_field_tasks(self):
+        self.transfer_data.transfer_increment_data(
+            database=self.database,
+            table='field_tasks'
+        )
 
+    def transfer_meteo_data(self):
+        self.transfer_data.transfer_increment_data(
+            database=self.database,
+            table='hourly_meteo_data'
+        )
 
-
-
-
-
-
-
-
-
-
-    
-    
+    def transfer_agro_meteo_data(self):
+        self.transfer_data.transfer_increment_data(
+            database=self.database,
+            table='hourly_agro_meteo_data'
+        )
